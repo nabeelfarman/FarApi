c1bbf07bb3d36f6ab311ceba3284e49e455cfd3d
AdnanAli7 <adnanali1990@gmail.com>
2020-07-27 03:01:36 +0500

    changes by adnan

---
+++ Models/userRoles.cs
@@ -0,0 +1,10 @@
+﻿public class userRoles
+{
+    public long RoleID { get; set; }
+    public string RoleDisplayName { get; set; }
+    public string RoleDescription { get; set; }
+    public int IsActivated { get; set; }
+    public long FARTypeID { get; set; }
+    public long RoleDetailID { get; set; }
+    public string FAType { get; set; }
+}
