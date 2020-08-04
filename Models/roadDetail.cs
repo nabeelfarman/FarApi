f36fd9667f1f24e468a88f85877f6ab968a06c8f
nabeelfarman <knabeel380@gmail.com>
2020-08-04 23:13:26 +0500

    change

---
+++ Models/roadDetail.cs
@@ -0,0 +1,43 @@
+public class roadDetail
+{
+    public long fixedAssetID { get; set; }
+    public long accountsCatID { get; set; }
+    public long roadID { get; set; }
+    public long projectID { get; set; }
+    public long officeSecID { get; set; }
+    public long landMeasureTypeID { get; set; }
+    public string accountsCatagory { get; set; }
+    public string roadShortName { get; set; }
+    public string roadName { get; set; }
+    public string projectShortName { get; set; }
+    public string projectName { get; set; }
+    public string officeDescription { get; set; }
+    public string landMeasureType { get; set; }
+    public string constructionFom { get; set; }
+    public string dateofNationalization { get; set; }
+    public string constructionTo { get; set; }
+    public string remarks { get; set; }
+    public int areaAcquiredKanal { get; set; }
+    public int areaAcquiredMarla { get; set; }
+    public int areaTransferedKanal { get; set; }
+    public long constructionCost { get; set; }
+    public long costofLand { get; set; }
+    public long openingCost { get; set; }
+    public long totalCost { get; set; }
+    public long openingRevaluationAmount { get; set; }
+    public long openingRevaluationSurplus { get; set; }
+    public long totalRevaluationAmount { get; set; }
+    public long revaluationSurplus { get; set; }
+    public long WDV { get; set; }
+    public long AdditionINRevaluationAmount { get; set; }
+    public long AdditionINRevaluationSurplus { get; set; }
+    public long openingAccDepreciation { get; set; }
+    public long accDepreciation { get; set; }
+    public long OWDV { get; set; }
+    public long roadLength { get; set; }
+    public long historicalCost { get; set; }
+    public float oProporationforCosttoWDV { get; set; }
+    public float proporationforCosttoWDV { get; set; }
+    public int areaTransferedMarla { get; set; }
+    public int isDeleted { get; set; }
+}
---
+++ Models/sudRoad.cs
@@ -0,0 +1,52 @@
+public class sudRoad
+{
+    public long AccountsCatID { get; set; }
+    public long OfficeSecID { get; set; }
+    public long ProjectID { get; set; }
+    public long RoadId { get; set; }
+    public long BuildingID { get; set; }
+    public string DateofNationalization { get; set; }
+    public string ConstructionFrom { get; set; }
+    public string ConstructionTo { get; set; }
+    public float ConstructionCost { get; set; }
+    public int LandMeasureTypeID { get; set; }
+    public float AreaAcquiredKanal { get; set; }
+    public float AreaAcquiredMarla { get; set; }
+    public float AreaTransferedKanal { get; set; }
+    public float AreaTransferedMarla { get; set; }
+    public float CostOfLand { get; set; }
+    public string Remarks { get; set; }
+    public string PurposeofPurchase { get; set; }
+    public string PresentUse { get; set; }
+    public long FixedAssetID { get; set; }
+    public long UserId { get; set; }
+    public string SpType { get; set; }
+
+
+
+
+    public string AccountsCatagory { get; set; }
+    public string RoadShortName { get; set; }
+    public long RoadLength { get; set; }
+    public string ProjectShortName { get; set; }
+    public string ProjectName { get; set; }
+    public string OfficeDescription { get; set; }
+    public string LandMeasureType { get; set; }
+    public float Openingcost { get; set; }
+    public float TotalCost { get; set; }
+    public float OpeningRevaluationAmount { get; set; }
+    public float OpeningRevaluationSurplus { get; set; }
+    public float TotalRevaluationAmount { get; set; }
+    public float RevaluationSurplus { get; set; }
+    public float WDV { get; set; }
+    public float ProporationforCosttoWDV { get; set; }
+    public float AdditionINRevaluationAmount { get; set; }
+    public float AdditionINRevaluationSurplus { get; set; }
+    public float OpeningAccDepreciation { get; set; }
+    public float AccDepreciation { get; set; }
+    public float OWDV { get; set; }
+    public float OProporationforCosttoWDV { get; set; }
+    public float historicalCost { get; set; }
+    public float Revaluation { get; set; }
+    public float AdditioninRevaluation { get; set; }
+}
