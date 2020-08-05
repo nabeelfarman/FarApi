ce83c1334077f43d4d620fcaeaf3d599aebf6f1e
nabeelfarman <knabeel380@gmail.com>
2020-08-05 18:03:24 +0500

    change

---
+++ Controllers/BuildConController.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace FarApi.Controllers
+{
+    public class BuildConController : ControllerBase
+    {
+
+        /*** DB Connection ***/
+        // static string dbCon = "Server=tcp:95.217.206.195,1433;Initial Catalog=FAR;Persist Security Info=False;User ID=sa;Password=telephone@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;";
+        static string dbCon = "Server=tcp:58.27.164.136,1433;Initial Catalog=FAR;Persist Security Info=False;User ID=far;Password=telephone@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;";
+        // static string dbCon = "Server=tcp:125.1.1.244,1433;Initial Catalog=FAR;Persist Security Info=False;User ID=far;Password=telephone@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;";
+
+
+        [Route("api/getBuildings")]
+        [HttpGet]
+        [EnableCors("CorePolicy")]
+        public IEnumerable<building> getBuildings()
+        {
+            List<building> rows = new List<building>();
+
+            using (IDbConnection con = new SqlConnection(dbCon))
+            {
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+
+                rows = con.Query<building>("select * from View_buildings order by buildingID desc").ToList();
+
+            }
+
+            return rows;
+        }
+
+        [Route("api/sudBuildCon")]
+        [HttpPost]
+        [EnableCors("CorePolicy")]
+        public IActionResult sudBuildCon([FromBody] building obj)
+        {
+            //
+            //***** Try Block
+            try
+            {
+                //****** Declaration
+                int rowAffected = 0;
+                string sqlResponse = "";
+                IActionResult response = Unauthorized();
+
+                using (IDbConnection con = new SqlConnection(dbCon))
+                {
+                    if (con.State == ConnectionState.Closed)
+                        con.Open();
+
+                    DynamicParameters parameters = new DynamicParameters();
+
+                    parameters.Add("@BuildingShortName", obj.buildingShortName);
+                    parameters.Add("@BuildingDescription", obj.buildingDescription);
+                    parameters.Add("@BuildingAddress", obj.buildingAddress);
+                    parameters.Add("@SubLocID", obj.subLocID);
+                    parameters.Add("@projectId", obj.projectID);
+                    parameters.Add("@PackageName", obj.packageName);
+                    parameters.Add("@BuildingID", obj.buildingId);
+                    parameters.Add("@Userid", obj.userID);
+                    parameters.Add("@SpType", obj.spType);
+                    parameters.Add("@ResponseMessage", dbType: DbType.String, direction: ParameterDirection.Output, size: 5215585);
+
+                    rowAffected = con.Execute("dbo.Sp_Buildings", parameters, commandType: CommandType.StoredProcedure);
+                    sqlResponse = parameters.Get<string>("@ResponseMessage");
+
+                }
+
+                response = Ok(new { msg = sqlResponse });
+
+                return response;
+
+            }
+            //***** Exception Block
+            catch (Exception ex)
+            {
+                return Ok(new { msg = ex.Message });
+            }
+        }
+
+    }
+}
---
+++ Models/building.cs
@@ -0,0 +1,17 @@
+public class building
+{
+    public long buildingId { get; set; }
+    public string buildingShortName { get; set; }
+    public string buildingDescription { get; set; }
+    public string projectShortName { get; set; }
+    public long projectID { get; set; }
+    public string projectName { get; set; }
+    public string buildingAddress { get; set; }
+    public long subLocID { get; set; }
+    public string packageName { get; set; }
+    public long accountCode { get; set; }
+    public string subLocationDescription { get; set; }
+    public string spType { get; set; }
+    public long userID { get; set; }
+    public string officeTypeDescription { get; set; }
+}
