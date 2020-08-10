08466e6e4e2b0b989cf105637099d18c6a0c4724
nabeelfarman <knabeel380@gmail.com>
2020-08-10 22:34:42 +0500

    change

--- Models/subLocationsDetail.cs
+++ Models/subLocationsDetail.cs
@@ -9,6 +9,7 @@
     public int isActivated { get; set; }
     public int isCompleted { get; set; }
     public string OfficeTypeDescription { get; set; }
+    public string completedDatetime { get; set; }
     public int OfficeTypeID { get; set; }
     public int TagsCreated { get; set; }
     public int SeqNo { get; set; }
