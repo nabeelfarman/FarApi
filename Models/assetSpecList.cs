b64ba810c57021a1c050fb0b636ced7b41f97e42
nabeelfarman <knabeel380@gmail.com>
2020-08-18 01:52:18 +0500

    change

---
+++ Models/assetSpecList.cs
@@ -0,0 +1,19 @@
+public class assetSpecList
+{
+    public int specID { get; set; }
+    public string specificationTitle { get; set; }
+    public string specificationNgModel { get; set; }
+    public string specificationListName { get; set; }
+    public string type { get; set; }
+    public int isDeleted { get; set; }
+    public int userID { get; set; }
+    public string createDateTime { get; set; }
+    public int specDetailID { get; set; }
+    public string assetCatDescription { get; set; }
+    public string specificationTitleSaved { get; set; }
+    public int assetCatID { get; set; }
+    public int makeID { get; set; }
+    public string makeTitle { get; set; }
+    public string No { get; set; }
+    public string spType { get; set; }
+}
